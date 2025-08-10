4fe624cf223d57d5744c0480436ee25a813843b3
Francis <franciskarani15@gmail.com>
2025-08-10 23:44:14 +0300

    initial commit

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,35 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = ""  # Use a private variable
+        self.value = value  # Will go through setter validation
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+            self._value = ""
+        else:
+            self._value = new_value
+
+    def is_sentence(self):
+        """Return True if value ends with a period."""
+        return self._value.endswith(".")
+
+    def is_question(self):
+        """Return True if value ends with a question mark."""
+        return self._value.endswith("?")
+
+    def is_exclamation(self):
+        """Return True if value ends with an exclamation mark."""
+        return self._value.endswith("!")
+
+    def count_sentences(self):
+        """Return number of sentences in value."""
+        import re
+        sentences = re.split(r'[.!?]', self._value)
+        sentences = [s.strip() for s in sentences if s.strip()]
+        return len(sentences)
